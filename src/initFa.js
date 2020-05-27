4262cd72d53991147b00102f9660bb72364978db
jordan meta <irnawatyully@gmail.com>
2020-05-27 23:04:41 +0800

    code splitting

--- src/initFa.js
+++ src/initFa.js
@@ -1,23 +1,17 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { 
-    faCheckSquare, 
-    faCoffee, 
-    faTable, 
-    faDatabase,
-    faVials,
-    faTasks,
-    faUsers,
-    faPencilAlt,
-    faTrash,
-    faCaretUp,
-    faCaretDown,
-    faCaretLeft,
-    faPlayCircle,
-    faWindowClose
-} from '@fortawesome/free-solid-svg-icons'
- 
+import { faCoffe } from '@fortawesome/free-solid-svg-icons/faCoffee'
+import { faTable } from '@fortawesome/free-solid-svg-icons/faTable'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons/faDatabase'
+import { faVials } from '@fortawesome/free-solid-svg-icons/faVials'
+import { faUsers } from '@fortawesome/free-solid-svg-icons/faUsers'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown'
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons/faCaretLeft'
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons/faPlayCircle'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons/faWindowClose'
+
 library.add(
-    faCheckSquare, 
     faCoffee, 
     faTable, 
     faDatabase,
